7542ec935c6539e679c8b6bb6916846447ea89c6
Wade Hammes <wade.hammes@gmail.com>
2015-03-10 13:16:56 -0400

    Removed hiding of elements, new overlay, updates for FB chat - v1.1.2

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -13,14 +13,19 @@ chrome.runtime.sendMessage({}, function(response) {
         if (document.readyState === "complete") {
             clearInterval(readyStateCheckInterval);
 
+            var bodyElement = document.getElementsByTagName('body')[0];
             var nubContainer = document.getElementsByClassName('rNubContainer');
             var toggleChatDiv = document.createElement('div');
+            var divOverlay = document.createElement('div');
 
             toggleChatDiv.setAttribute('class','uiToggle _50-v fbNub _4up toggleChat');
             toggleChatDiv.setAttribute('id', 'toggleChat');
             toggleChatDiv.innerHTML = '<b>Hide</b> Facebook';
 
+            divOverlay.setAttribute('class','distractionFree-overlay');
+
             nubContainer[0].appendChild(toggleChatDiv);
+            bodyElement.appendChild(divOverlay);
 
             document.getElementById('toggleChat').onclick = toggleClass;
         }
